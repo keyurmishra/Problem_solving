e2ba8fda82d07cfb59389e9a73d86c339eba4a8d
Keyur Mishra <keyurmishra235@gmail.com>
2026-07-06 10:47:17 +0530

    Time: 3 ms (65.75%), Space: 8.3 MB (10.61%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
-    vector<int>dp=vector<int>(31,-1);// size must be max constraint 
-    int fib(int n) {
-        if(n==0 || n==1){
-            return n;
-        }
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=fib(n-1)+fib(n-2);
+    // vector<int>dp=vector<int>(31,-1);// size must be max constraint 
+    // int fib(int n) {
+    //     if(n==0 || n==1){
+    //         return n;
+    //     }
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n]=fib(n-1)+fib(n-2);
         
+    // }
+    int fib(int n){
+        vector<int>dp(n+1);
+        dp[0]=0;
+        if(n>=1)dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
