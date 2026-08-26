84bb17078db9dc88507751702f481bbdb60a8082
Keyur Mishra <keyurmishra235@gmail.com>
2026-08-27 03:01:08 +0530

    Time: 4 ms (26.43%), Space: 25.1 MB (24.66%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_set<int>st(nums.begin(),nums.end());
-        int multiple=k;
-        while(st.count(multiple)){
-            multiple+=k;
+        // unordered_set<int>st(nums.begin(),nums.end());
+        // int multiple=k;
+        // while(st.count(multiple)){
+        //     multiple+=k;
+        // }
+        // return multiple;
+        unordered_set<int>mp(nums.begin(),nums.end());
+        int mul=k;
+        while(mp.count(mul)){
+            mul+=k;
         }
-        return multiple;
+        return mul;
         
     }
 };
