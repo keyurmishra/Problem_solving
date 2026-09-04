b752d17750917420a660855ff16043fdd7e1f94b
Keyur Mishra <keyurmishra235@gmail.com>
2026-09-05 04:19:45 +0530

    Time: 0 ms (100%), Space: 31.3 MB (46.71%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        // here we have to print the index value which is smaller and equal to the k
+        // // here we have to print the index value which is smaller and equal to the k
+        // int n=nums.size();
+        // // actually this is the concept of the prefix and the suffix methode 
+        // if(n==0) return -1;
+        // vector<int>suffixmin(n);
+        // suffixmin[n-1]=nums[n-1];//provding the firdt ele 
+        // for(int i=n-2;i>=0;i--){
+        //     suffixmin[i]=min(nums[i],suffixmin[i+1]);
+        // }
+        // int currmax=nums[0];
+        // for(int i=0;i<n;i++){
+        //     currmax=max(currmax,nums[i]);
+        //     int diff=currmax - suffixmin[i] ;
+        //     if(diff<=k){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        //HERE I FOUND THE SMALLEST FROM THE BACK AND MAXI FFROM THE FRONT 
         int n=nums.size();
-        // actually this is the concept of the prefix and the suffix methode 
-        if(n==0) return -1;
+        int prefixmax=0;
         vector<int>suffixmin(n);
-        suffixmin[n-1]=nums[n-1];//provding the firdt ele 
+        suffixmin.back()=nums.back();
         for(int i=n-2;i>=0;i--){
             suffixmin[i]=min(nums[i],suffixmin[i+1]);
         }
-        int currmax=nums[0];
         for(int i=0;i<n;i++){
-            currmax=max(currmax,nums[i]);
-            int diff=currmax - suffixmin[i] ;
-            if(diff<=k){
-                return i;
-            }
+            prefixmax=max(prefixmax,nums[i]);
+            if(prefixmax-suffixmin[i]<=k)return i;
         }
         return -1;
         
